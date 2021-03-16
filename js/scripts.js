a61656c76e52528790eaabb290bedd74bbd9a857
bfab11 <79426655+bfab11@users.noreply.github.com>
2021-03-15 22:35:35 -0700

    1.2 Delete existing code and added pokemonList array and 3 objects
    Deleted the existing code from the previous task and set pokemonList array empty, set 3 objects and added them to the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
-alert('Hello world');
+let pokemonList = [];
 
-let favoriteFood = 'sushi';
-document.write(favoriteFood);
+let pokemon1 = {
+  name: 'Bulbasaur',
+  height: 0.7,
+  type: 'Grass/Poison'
+};
+
+let pokemon2 = {
+  name: 'Ivysaur',
+  height: 1,
+  type: 'Grass/Poison'
+};
+
+let pokemon3 = {
+  name: 'Venasaur',
+  height: 2,
+  type: 'Grass/Poison'
+};
+
+pokemonList.push(pokemon1, pokemon2, pokemon3);
