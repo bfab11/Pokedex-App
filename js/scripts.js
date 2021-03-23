3974e504fb0dda2f6844b54c5b8f0fe8acb60c11
bfab11 <79426655+bfab11@users.noreply.github.com>
2021-03-22 22:02:27 -0700

    1.5 part 2 rewrote code to create IIFE pokemonRepository and forEach loop to call getAll function from pokemonRepository

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,41 @@
-//Create pokemonList array
-let pokemonList = [];
+// IIFE pokemonRepository
+let pokemonRepository = (function () {
+  //Create pokemonList array
+  let pokemonList = [];
 
-//Define pokemon objects
-let pokemon1 = {
-  name: 'Bulbasaur',
-  height: 0.7,
-  type: 'Grass/Poison'
-};
+  //Define pokemon objects
+  let pokemon1 = {
+    name: 'Bulbasaur',
+    height: 0.7,
+    type: 'Grass/Poison'
+  };
 
-let pokemon2 = {
-  name: 'Ivysaur',
-  height: 1,
-  type: 'Grass/Poison'
-};
+  let pokemon2 = {
+    name: 'Ivysaur',
+    height: 1,
+    type: 'Grass/Poison'
+  };
 
-let pokemon3 = {
-  name: 'Venasaur',
-  height: 2,
-  type: 'Grass/Poison'
-};
+  let pokemon3 = {
+    name: 'Venasaur',
+    height: 2,
+    type: 'Grass/Poison'
+  };
 
-//Add pokemon objects into pokemonList array
-pokemonList.push(pokemon1, pokemon2, pokemon3);
+  //Add pokemon objects into pokemonList array
+  pokemonList.push(pokemon1, pokemon2, pokemon3);
 
-pokemonList.forEach(function(pokemon) {
+  return {
+    add: function(pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function() {
+      return pokemonList;
+    }
+  };
+})();
+
+ pokemonRepository.getAll().forEach(function(pokemon) {
   let pokemonPrint = pokemon.name + '[' + pokemon.type + '] ' + '(height: ' + pokemon.height + ')';;
 
   if (pokemon.height > 1){
